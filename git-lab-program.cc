ab234bdebc57654bc135ecebaef6fb89fa257f98
Hayden Kind <hk969414@ohio.edu>
2021-01-27 19:21:28 -0500

    first commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Hayden Kind
+ *        Date: January 27th, 2021
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
